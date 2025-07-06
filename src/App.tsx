0b0eeb7cdd379319a4af0eb234831653a7fb81a7
Sherhan <kantaevsherhan6@gmail.com>
2025-07-06 12:45:01 +0500

    save

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import type { IHeaderNavItem } from "./types/header";
 export const App = () => {
   const headerNavItems: IHeaderNavItem[] = [
     {
-      title: "Home",
-      href: "#home",
-    },
-    {
       title: "About",
       href: "#about",
+    },
+    {
+      title: "Skills",
+      href: "#skills",
     },
     {
       title: "Projects",
@@ -22,20 +22,29 @@ export const App = () => {
   ];
 
   return (
-    <div className="app">
-      <div className="profile">
+    <div className="app py:50px">
+      <div className="profile box-shadow:#00000001|0px|4px|12px">
         <div className="container">
           <div className="inner bg:#fff">
             <Header navItems={headerNavItems} />
 
             <section className="intro">
               <div className="inner-container">
-                <div className="left">
-                  <p>Hello,</p>
-                  <h1>Kantaev Sherhan</h1>
-                  <h3>a Full-stack developer</h3>
+                <div className="flex flex:row ai:center jc:space-between">
+                  <div className="left">
+                    <p className="f:24px">Hello,</p>
+                    <h1 className="f:56px f:heavy">Kantaev Sherhan</h1>
+                    <h3 className="f:32px">a Full-stack developer</h3>
+                  </div>
+
+                  <div className="right">
+                    <img
+                      src="/profile-intro.jpg"
+                      alt="Kantaev Sherhan"
+                      className="profile-image h:430px w:430px round object:cover"
+                    />
+                  </div>
                 </div>
-                <div className="right"></div>
               </div>
             </section>
           </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,15 +7,24 @@ interface IHeaderProps {
 
 export const Header: FC<IHeaderProps> = ({ navItems }) => {
   return (
-    <header className="app-header">
+    <header className="app-header py:45px">
       <div className="inner-container">
-        <nav className="header-navigation">
-          {navItems.map((item) => (
-            <a href={item.href} className="text:none">
-              {item.title}
-            </a>
-          ))}
-        </nav>
+        <div className="inner flex flex:row">
+          <div className="logo flex jc:center ai:center f:21px f:bold w:45px h:45px bg:#333 f:#fff round mr:60px">
+            KS
+          </div>
+
+          <nav className="header-navigation flex flex:row ai:center gap:45px">
+            {navItems.map((item) => (
+              <a
+                href={item.href}
+                className="text:none text:underline:hover f:18px f:semibold f:#333 opacity:0.8 opacity:1:hover transition:opacity:0.3s"
+              >
+                {item.title}
+              </a>
+            ))}
+          </nav>
+        </div>
       </div>
     </header>
   );
